07f8a0ca179e792e8f43ede3d70636d50d5619be
Lukasz Grzegorz Maciak <maciakl@gmail.com>
2012-01-10 23:37:31 -0500

    Housekeeping: deleted bunch of redundant files.

--- SetupAssistant/1MERGE_SA.cmd
+++
@@ -1,3 +0,0 @@
-@ilmerge /out:"C:\Users\luke\Documents\My Dropbox\Programming\SetupAssistant\SetupAssistant\bin\Debug\Setup-Assistant.exe" "C:\Users\luke\Documents\My Dropbox\Programming\SetupAssistant\SetupAssistant\bin\Debug\SetupAssistant.exe" "C:\Users\luke\Documents\My Dropbox\Programming\SetupAssistant\SetupAssistant\bin\Debug\Ionic.Zip.dll" /log
-
-@pause
--- SetupAssistant/2MERGE_SA_onvm.cmd
+++
@@ -1,3 +0,0 @@
-@ilmerge /out:"E:\My Dropbox\Programming\SetupAssistant\SetupAssistant\bin\Debug\Setup-Assistant.exe" "E:\My Dropbox\Programming\SetupAssistant\SetupAssistant\bin\Debug\SetupAssistant.exe" "E:\My Dropbox\Programming\SetupAssistant\SetupAssistant\bin\Debug\Ionic.Zip.dll" /log
-
-@pause
