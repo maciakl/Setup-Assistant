484aa9ff2666855f5e7cb21f240615f5a9e70ab5
Lukasz Grzegorz Maciak <maciakl@gmail.com>
2012-05-03 20:24:17 -0400

    Modified the post build scripts.
    Change the MERGE script to auto-generate a zipfile for upload.
    Added the contents of the script to the VS post-build box to automate it.

--- SetupAssistant/SetupAssistant.cs
+++ SetupAssistant/SetupAssistant.cs
@@ -1975,8 +1975,8 @@ please file a bug report at http://sa.maciak.net";
             */
             string contents = "Dim FSO\r\n";
             contents += "Set FSO = CreateObject(\"Scripting.FileSystemObject\")\r\n";
+            contents += "WScript.sleep 480\r\n";
             contents += "FSO.CopyFile \""+ from_path +"\", \""+to_path+"\", True\r\n";
-            contents += "WScript.sleep 360\r\n";
             contents += "Set wshShell = WScript.CreateObject (\"WSCript.shell\")\r\n";
             contents += "WshShell.run \"\"\"" + to_path + "\"\"\"\r\n";
             
--- SetupAssistant/bin/Debug/====MERGE====.cmd
+++ SetupAssistant/bin/Debug/====MERGE====.cmd
@@ -1,3 +1,5 @@
+@del Setup-Assistant.exe
 @ilmerge /out:"Setup-Assistant.exe" "SetupAssistant.exe" "Ionic.Zip.dll" /log
-
+@del SetupAssistant.zip
+@zip -9 SetupAssistant.zip Setup-Assistant.exe
 @pause
