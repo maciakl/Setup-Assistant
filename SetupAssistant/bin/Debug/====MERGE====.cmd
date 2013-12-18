9b1ce87f99704dfabfb1134881d2bd1a48532b7b
Lukasz Grzegorz Maciak <maciakl@gmail.com>
2013-12-17 21:44:34 -0500

    Ver 1.9

--- SetupAssistant/bin/Debug/====MERGE====.cmd
+++ SetupAssistant/bin/Debug/====MERGE====.cmd
@@ -1,5 +1,3 @@
 @del Setup-Assistant.exe
-@ilmerge /out:"Setup-Assistant.exe" "SetupAssistant.exe" "Ionic.Zip.dll" /log
-@del SetupAssistant.zip
-@zip -9 SetupAssistant.zip Setup-Assistant.exe
+@"C:\Program Files (x86)\Microsoft\ILMerge\ilmerge" /targetplatform:v4,"C:\Windows\Microsoft.NET\Framework\v4.0.30319" /out:"Setup-Assistant.exe" "SetupAssistant.exe" "Ionic.Zip.dll" /log
 @pause
